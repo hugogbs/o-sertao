befbdd97e7e6283687165800cccef6fede6cf431
Gabriel <hugo.silva@ccc.ufcg.edu.br>
2017-03-22 22:02:25 -0300

    Finalizações do mapa 1

--- js/mapa1.js
+++ js/mapa1.js
@@ -12,7 +12,7 @@
         .center([-41.083760, -9.309294])
         .rotate([0, 0])
         .parallels([0, 0])
-        .scale(12000);
+        .scale(1800);
 
           g = svg.append("g")
         .attr("transform", "translate(20,20)");
@@ -24,63 +24,92 @@
         var path = d3.geoPath().projection(projection);
 
         var color = d3.scaleThreshold();
-        var subtitleScale = d3.scaleOrdinal([d3.rgb(0, 96, 100, 1.0), d3.rgb(0, 96, 100, 0.8), d3.rgb(0, 96, 100, 0.6), d3.rgb(0, 96, 100, 0.4), d3.rgb(0, 96, 100, 0.2), "#FFAB91"]);
+        var subtitleScale = 
+            d3.scaleOrdinal([d3.rgb(204, 0, 0, 1.0), 
+                            d3.rgb(204, 0, 0, 0.8), 
+                            d3.rgb(204, 0, 0, 0.6), 
+                            d3.rgb(204, 0, 0, 0.4), 
+                            d3.rgb(204, 0, 0, 0.2)]);
+
+
 
         d3.queue()
-            .defer(d3.json, "data/municipios_sab.json")
-            .defer(d3.csv, "data/municipios_sab.csv")
+            .defer(d3.json, "https://rawgit.com/hugogbs/o-sertao/master/data/municipios_sab.json")
+            .defer(d3.csv,  "https://raw.githubusercontent.com/hugogbs/o-sertao/master/data/municipios_sab.csv")
             .await(draw);
 
         function draw(error, sab, dados_sab) {
             if (error) throw error;
 
-            // desenhando municipios
 
+            // desenhando municipios
             var municipios = topojson.feature(sab, sab.objects.municipios_sab);
 
+            var text = svg.append("text").attr('class', 'label');
+
             color
-                .domain([20, 40, 60, 80, 100])
-                .range([d3.rgb(0, 96, 100, 1.0), d3.rgb(0, 96, 100, 0.8), d3.rgb(0, 96, 100, 0.6), d3.rgb(0, 96, 100, 0.4), d3.rgb(0, 96, 100, 0.2)]);
+                .domain([0, 10000, 20000, 30000, 40000, 50000])
+                .range([d3.rgb(204, 0, 0, .2), d3.rgb(204, 0, 0, .4), d3.rgb(204, 0, 0, .6), d3.rgb(204, 0, 0, .8), d3.rgb(204, 0, 0, 1.0)]);
 
 
-
-              svg.selectAll(".municipios")
+            svg.selectAll(".municipios")
                 .data(municipios.features)
                 .enter()
                 .append("path")
                 .attr("d", path)
+                .attr("id", function(d) { return "area-"+d.properties.ID.toString()})
                 .attr('class', 'municipio')
-                .attr("id", function(d) { return "area-"+d.properties.ID; });
+                .on("mouseover", function(d){ 
+                  text
+                  .attr('x', 100)
+                  .attr('y', 100)
+                  .attr("transform", "translate(10, 25)")
+                  .text(getData(d.properties.ID).MUNICIPIO + " (" + 
+                    getData(d.properties.ID).UF  + '), ' +  
+                    getData(d.properties.ID).POPULACAO_TOTAL +  ' habitantes')
+                });
 
-                for (var i = 0; i < dados_sab.length; i++) {
-                    svg.select("#area-"+dados_sab[i].GEOCODIGO1)
-                        .attr("fill", function() {
-                            if (isNaN(dados_sab[i].POPULACAO_URBANA)) {
-                                return "#FFAB91";
-                            } else {
-                                 return color(+dados_sab[i].POPULACAO_URBANA);
-                            }
-                        })
-                }
+              for (var i = 0; i < dados_sab.length; i++) {
+                 cidade = dados_sab[i]
+                 svg.select("#area-"+cidade.GEOCODIGO )
+                  .attr("fill", function() {
+                if (isNaN(cidade.POPULACAO_URBANA)) {
+                   return "#3333CC";
+                } else {
+                   return color(+cidade.POPULACAO_URBANA);
+                 }
+                })
+              }
 
-                  var legend = g.selectAll(".legend")
-                    .data(["Entre 81% e 100%", "Entre 61% e 80%", "Entre 41% e 60%", "Entre 21% e 40%", "20% ou menos", "Não Há Dados"])
-                    .enter().append("g")
-                      .attr("class", "legend")
-                      .attr("transform", function(d, i) { return "translate(-" + 220 + ", " + (i+5) * 20 + ")"; })
-                      .style("font", "14px sans-serif");
+              var legend = g.selectAll(".legend")
+                .data(["Entre 40001 e 50000", "Entre 30001 e 40000", "Entre 20001 e 30000", "Entre 10001 e 20000", "Entre 0 e 10000"  ])
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function(d, i) { return "translate(-" + 220 + ", " + (i+5) * 20 + ")"; })
+                .style("font", "14px sans-serif");
 
-                  legend.append("rect")
-                      .attr("x", width + 18)
-                      .attr("width", 18)
-                      .attr("height", 18)
-                      .attr("fill", subtitleScale);
+                legend.append("rect")
+                .attr("x", width + 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .attr("fill", subtitleScale);
 
-                  legend.append("text")
-                      .attr("x", width + 44)
-                      .attr("y", 9)
-                      .attr("dy", ".35em")
-                      .attr("text-anchor", "start")
-                      .text(function(d) { return d; });
+                legend.append("text")
+                .attr("x", width + 44)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .attr("text-anchor", "start")
+                .text(function(d) { return d; });
+    
+    function getData(id){
+      for (var i = 0 ; i <dados_sab.length; i++){      
+        if (dados_sab[i].GEOCODIGO == id.toString()){
+          return dados_sab[i];
+          break;
+        }
+      }
     }
+  }
+
+
 
